6a65480af215b2b4a6a941855cc790bca52f5fc9
ntienthanh <ntienthanh@MacBook-Pro-6.local>
2024-05-26 15:08:26 +1000

    hfhfh

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ let dots = document.querySelectorAll('.slider .dots li');
 
 let lengthItems = items.length - 1;
 let active = 0;
+let temp = 1;
 next.onclick = function(){
+    // active.style.opacity = '0';
     active = active + 1 <= lengthItems ? active + 1 : 0;
+
     reloadSlider();
 }
 prev.onclick = function(){
@@ -18,15 +21,13 @@ prev.onclick = function(){
 let refreshInterval = setInterval(()=> {next.click()}, 8000);
 function reloadSlider(){
     slider.style.left = -items[active].offsetLeft + 'px';
-    // 
     let last_active_dot = document.querySelector('.slider .dots li.active');
     last_active_dot.classList.remove('active');
     dots[active].classList.add('active');
 
     clearInterval(refreshInterval);
     refreshInterval = setInterval(()=> {next.click()}, 8000);
-
-    
+ 
 }
 
 dots.forEach((li, key) => {
@@ -38,3 +39,8 @@ dots.forEach((li, key) => {
 window.onresize = function(event) {
     reloadSlider();
 };
+
+
+
+
+
